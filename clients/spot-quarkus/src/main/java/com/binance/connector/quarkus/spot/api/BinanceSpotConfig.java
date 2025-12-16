5384c9271a3b9352c91ad21db37250052c883c47
Oleh Bilous <boa070270@gmail.com>
2025-12-16 14:57:10 +0200

    fix config

--- clients/spot-quarkus/src/main/java/com/binance/connector/quarkus/spot/api/BinanceSpotConfig.java
+++ clients/spot-quarkus/src/main/java/com/binance/connector/quarkus/spot/api/BinanceSpotConfig.java
@@ -4,14 +4,12 @@ import io.smallrye.config.ConfigMapping;
 
 import java.util.List;
 
-@ConfigMapping(prefix = "binance.spot")
 public interface BinanceSpotConfig {
     boolean useTestNet();
 
     String endpointApi();
 
     String endpointTest();
-    List<String> symbols();
 
     String storeKey();
 }
